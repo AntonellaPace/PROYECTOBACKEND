323da01d52140ec725ac1947fa0087abe6181939
Antonella Pace <anto.pace.escoto@gmail.com>
2024-02-17 19:46:00 -0300

    entrega 3

--- OneDrive/Escritorio/BACKEND2/ProductManager.js
+++ OneDrive/Escritorio/BACKEND2/ProductManager.js
@@ -1,5 +1,108 @@
 import fs from "fs"
 
+export default class ProductManager {
+    constructor() {
+      this.products = [];
+      this.id = 1;
+      this.path = "./products.json";
+
+    }
+
+    addProduct = async (title, description, price, img, code, stock) => {
+
+        const required = [title, description, price, img, code, stock];
+            if (!required.every(field => field)) {
+                console.log( "Todos los campos son obligatorios");
+                return;
+            }
+
+            if (this.products.some(product => product.code === code)) {
+                console.log(" Ya existe un producto con el ese código");
+                return;
+            }
+
+        const newProduct = {
+            id: this.id,
+            title,
+            description,
+            price,
+            img,
+            code,
+            stock
+        };
+        this.products.push(newProduct);
+        this.id++;
+        console.log(" producto se agrego correctamente:", newProduct);
+
+        try {
+            await fs.writeFile(this.path, JSON.stringify(this.products, null, 2));
+        }catch (error) {
+            console.error("no se pudo guardar el producto:", error);
+            throw error;
+        }        
+    };
+
+    getProducts = async () => {
+        try{
+            const items = await fs.readFile(this.path, "utf-8");
+            console.log(items);
+            return JSON.parse(items);
+        }catch (error) {
+            console.error(error);
+            throw error;
+        }
+    };
+
+    getProductById = async (id) => {
+        try{
+            const products = await this.getProducts()
+        const product = products.find(product => product.id === id);
+        if (product) {
+            return product;
+        };
+        }catch (error) {
+            console.error(error);
+            throw error;
+        }
+    };
+
+    updateProduct = async (productId, updatedFields) => {
+        try {
+            const products = await this.getProducts();
+            const productIndex = products.findIndex(product => product.id === productId);
+            if (productIndex === -1) {
+                console.log(' No se encontró ningún producto con el ID');
+                return;
+            }
+            products[productIndex] = { ...products[productIndex], ...updatedFields };
+            await fs.writeFile(this.path, JSON.stringify(products, null, 2));
+        } catch (error) {
+            console.error("Error al actualizar el producto:", error);
+            throw error;
+        }  
+    };
+
+// Elimina un producto por su ID
+    deleteId = async (id) => {
+        try {
+            let found = false;
+            const items = await this.getProducts();
+            const deleteId = items.filter(i => {
+            if (i.id === id) found = true;
+                return (i.id != id);
+            });
+            console.log(`producto con ID: ${this.id} borrado `, deleteId);
+            if (found) {
+                await fs.writeFile("./products.json", JSON.stringify(deleteId, null, 2));
+            }
+        } catch (error) {
+            console.error("Error al eliminar el producto:", error);
+            throw error;
+        }
+    };
+};
+
+/*
 class ProductManager {
     constructor() {
       this.products = [];
@@ -75,7 +178,7 @@ class ProductManager {
 
 //Proceso de Testing:
 
-const newProductManager = new ProductManager();
+const pm = new ProductManager();
 
 // Crear productos
 
@@ -109,4 +212,4 @@ newProductManager.updateProduct(3,{description: "descripcion del mueble de dormi
 
 let delId = 4;
 const deletedId = newProductManager.deleteId(delId);
-
+*/
---
+++ OneDrive/Escritorio/BACKEND2/app.js
@@ -0,0 +1,45 @@
+import express from 'express';
+
+import ProductManager from './ProductManager.js';
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended:true }));
+
+const pm = new ProductManager();
+
+app.get('/products', async (req, res) => {
+    try{
+        let limit = req.query.limit;
+        let products = await pm.getProducts();
+        if (limit) {
+            let prodLimit = products.slice(0, parseInt(limit));
+            res.send(prodLimit);
+        }else {
+            res.send(products);
+        }
+    }catch (error) {
+        res.status(418).send({error, message:"no funcionaa"});
+    }
+});
+
+app.get('/products/:pid', async (req, res) => {
+    try{
+        let ProdId = req.params.pid;
+        const product = await pm.getProductById(Number(ProdId));
+        if (product) {
+            res.send(product);
+        } else {
+            res.status(404).send({status: "error", message: "No se encuentra el producto con ese ID"});
+        };
+    } catch (error) {
+        res.status(418).send({error, message:"no funciona"})
+    }
+});
+
+//Iniciar server
+app.listen(PORT, () => {
+    console.log(`servidor funcionando en puerto ${PORT}.`);
+});
