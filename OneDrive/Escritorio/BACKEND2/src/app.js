19e43750e59e6d2592fb63abb056da3163b4c3cd
Antonella Pace <anto.pace.escoto@gmail.com>
2024-03-02 18:02:19 -0300

    desafio 5 terminado

--- OneDrive/Escritorio/BACKEND2/src/app.js
+++ OneDrive/Escritorio/BACKEND2/src/app.js
@@ -1,29 +1,3 @@
-/*import express from 'express';
-import cartRoutes from './routes/cart.routes.js';
-import productRoutes from './routes/products.routes.js';
-
-const app = express();
-const PORT = 8080;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended:true }));
-
-app.get('/', async (req, res) => {
-    try{
-        res.send("Bienvenido a ~GOMEZ&ASOCIADOS~")
-    }catch (error) {
-        res.status(418).send({error, message:"No funciona"});
-    }
-});
-
-app.use("/api/products", productRoutes);
-app.use('/api/carts', cartRoutes);
-
-app.listen(PORT, () => {
-    console.log(`Servidor funcionando en Puerto ${PORT}.`);
-});
-*/
-
 import express from "express";
 import http from "http";
 import exphbs from "express-handlebars";
